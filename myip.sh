30a383f947f1a6a13da5573b400ae436611c8428
dvlcube <ulisses@dvlcube.com>
2013-06-05 17:50:59 -0300

    dummy file creator

--- myip.sh
+++ myip.sh
@@ -5,6 +5,13 @@ SSH_USER=
 SSH_PASS=
 # only try ips greater than this number:
 PORT_GT=0
+DEBUG=0
+
+debug() {
+	if [ $DEBUG -gt 0 ]; then
+		echo "$1"
+	fi
+}
 
 while test $# -gt 0
 do
@@ -16,13 +23,17 @@ do
 		--password|-p) shift
 			SSH_PASS=$1
         ;;
+        --debug)
+        	DEBUG=1
+        	debug "debug is active"
+        ;;
 		--user|-u) shift
 			SSH_USER=$1
         ;;
 		--port) shift
 			SSH_PORT=$1			
         ;;
-        --gt|-g) shift
+        --start|--gt|-g) shift
         	PORT_GT=$1
         ;;
 		--*) echo "bad option $1"
@@ -35,14 +46,16 @@ done
 echo "mapping..."
 active_ips=`nmap -sP 192.168.0.0/24 | grep 192 | cut -d' ' -f5`
 echo "will try to find ip for user $SSH_USER with pass $SSH_PASS"
-if [ $PORT_GT > 0 ]; then
+if [ $PORT_GT -gt 0 ]; then
 	echo "Trying only ips greater than $PORT_GT"
 fi
 
 for ip in $active_ips
-do
+do		
 	ip_n=`echo $ip | cut -d. -f4`
-	if [[ $ip_n -gt $PORT_GT ]]; then
+	debug "$ip ($ip_n)"
+	if [[ $ip_n > $PORT_GT || ! -n $ip_n ]]; then
+		debug "ip passed > $PORT_GT || NaN test"
 		nc -z $ip $SSH_PORT 1>/dev/null 2>&1; result=$?;
 		if [ $result -eq 0 ]; then
 			echo "trying $ip..."
