2cc14e073e5097f24b24c000b4bdf6e6b1d31a1f
dvlcube <ulisses@dvlcube.com>
2013-07-23 11:51:01 -0300

    --clear, more help and exiting accordingly

--- dummy-file.sh
+++ dummy-file.sh
@@ -2,9 +2,20 @@
 
 if [ "$1" == '--help' ]; then
   echo "creates a temporary file in /tmp with $1 MB in size."
+  echo ""
   echo "example to create a temporay file with 50 MB:"
   echo "$0 50"
   echo "a file /tmp/dummy.50 will be created"
+  echo ""
+  echo "to free all the space created:"
+  echo "$0 --clear"
+  exit 0
+fi
+
+if [ "$1" == '--clear' ]; then
+  echo "reclaiming space..."
+  rm /tmp/dummy.*
+  exit 0
 fi
 
 mb=$1
