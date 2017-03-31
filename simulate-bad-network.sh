92e94fedb6f5cdd3c17f560a618bbd5e0c9f297e
Ulisses Lima <ulisses@dvlcube.com>
2017-03-31 11:59:04 -0300

    bad net

--- simulate-bad-network.sh
+++ simulate-bad-network.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+log=$HOME/logs/tc_qdisc-history.log
+mkdir -p `dirname $log`
+
 switch=${1:-add}
 interface=${2:-eth0}
 delay=${3:-50}
@@ -7,4 +10,7 @@ loss=${4:-1}
 
 echo "interface ${interface}, ${switch}: network delay of $delay ms with ${loss}% packet loss"
 
-tc qdisc $switch dev $interface root netem delay ${delay}ms loss ${loss}%
+c="tc qdisc $switch dev $interface root netem delay ${delay}ms loss ${loss}%"
+echo "$c"
+echo "$c" >> $log
+$c
