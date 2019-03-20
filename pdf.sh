f02b19d9632ec9c23a3c72a7ffa9bbbb0d90ea0f
Ulisses Lima <ulisses@dvlcube.com>
2019-03-20 13:20:40 -0300

    pdf.sh - convert text input to pdf

---
+++ pdf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+require() {
+    switch='-s'
+    if [[ "$1" == *'-'* ]]; then
+        switch=$1
+        shift
+    fi
+
+    case $switch in
+      --string|-s)
+        if [ ! -n "$1" ]; then
+            log "$2"
+            exit 1
+        fi
+      ;;
+      --file|-f)
+        if [ ! -f "$1" ]; then
+            log "Arquivo não encontrado: '$1'"
+            exit 1
+        fi
+      ;;
+      --dir|-d)
+        if [ ! -d "$1" ]; then
+            log "Diretório não encontrado: '$1'"
+            exit 1
+        fi
+      ;;
+    esac
+}
+
+now=$(date +'%Y-%m-%d_%H-%M-%S')
+fname="pdf-${now}"
+
+require -f $(which enscript)
+require -f $(which ps2pdf)
+
+enscript -p "${fname}.ps" /dev/stdin
+ps2pdf "${fname}.ps"
+
+rm "${fname}.ps"
