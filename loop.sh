77d8c4d2d268f41d6dbbc4ecc64ebe87a9c3102d
Ulisses Lima <ulisses@dvlcube.com>
2019-11-21 20:34:09 -0300

    loop.sh - loop any command

---
+++ loop.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+source /etc/profile
+
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+do_help() {
+	echo "usage:"
+	echo "$0"
+}
+
+interval=1
+max=1000
+command=
+i=1
+
+while test $# -gt 0
+do
+    case "$1" in
+	--help|-h)
+        	do_help
+        	exit 0
+      	;;
+      	--verbose|-v)
+        	verbose=true
+      	;;
+      	--interval|-i)
+      		shift
+      		interval="$1"
+      	;;
+      	--command|-c)
+      		shift
+      		command="$1"
+      	;;
+	--max|-m)
+		shift
+		max="$1"
+	;;
+      	--*)
+        	echo "bad option '$1'"
+		exit 1
+      	;;
+    esac
+    shift
+done
+
+while true
+do
+	echo "$(date) - $i"
+	echo "$command"
+	eval $command
+	((i+=1))
+	sleep $interval
+done
--- replace-all.sh
+++ replace-all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+silent=${4:-false}
+
 if [ ! -f "$3" ]; then
 	echo "third argument must be a file"
 fi
 
-echo "replacing '$1' with '$2' in '$3'"
+[[ $silent == false ]] && echo "replacing '$1' with '$2' in '$3'"
 sed -i "s/$1/$2/g" "$3"
