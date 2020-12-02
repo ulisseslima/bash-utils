c0212e3f0699d8c478e09198e22f4310af6bdec1
Ulisses Lima <ulisses@dvlcube.com>
2020-12-02 18:37:29 -0300

    simplified logs

--- require.sh
+++ require.sh
@@ -1,61 +1,40 @@
 #!/bin/bash
 
 function log() {
-    level="$1"
-    shift
-
-    indicator="$1"
-    shift
-
-    echo "$indicator $(now.sh -dt) - $level: $@"
-    echo "$MYSELF - $indicator $(now.sh -dt) - $level: $@" >> /tmp/general.log
-}
-
-function info() {
-    log INFO '###' "$@"
-}
-
-function err() {
-    >&2 log ERROR '!!!' "$@"
-}
-
-function debug() {
-    if [[ "$verbose" == true ]]; then
-        >&2 log DEBUG '>>>' "$@"
-    fi
+    >&2 echo "$@"
 }
 
 require() {
-        switch='-s'
-        if [[ "$1" == *'-'* ]]; then
-            switch=$1
-            shift
-        fi
+    switch='-s'
+    if [[ "$1" == *'-'* ]]; then
+        switch=$1
+        shift
+    fi
 
 	keyname="$1"
 	value="${!keyname}"
 	info="$2"
 
-        case $switch in
-          --string|-s)
-                if [ ! -n "$value" ]; then
-                    log "required variable has no value - $keyname: $info"
-                    return 1
-                fi
-          ;;
-          --file|-f)
-                    if [ ! -f "$value" ]; then
-                        log "an expected file was not found: '$value' (varname: $keyname) - $info"
-                        return 2
-                fi
-          ;;
-          --dir|-d)
-                if [ ! -d "$value" ]; then
-                    log "an expected dir was not found: '$value' (varname: $keyname) - $info"
-                    return 3
-                fi
-          ;;
-        esac
+    case $switch in
+        --string|-s)
+            if [ ! -n "$value" ]; then
+                log "required variable has no value - $keyname: $info"
+                exit 1
+            fi
+        ;;
+        --file|-f)
+                if [ ! -f "$value" ]; then
+                    log "an expected file was not found: '$value' (varname: $keyname) - $info"
+                    exit 2
+            fi
+        ;;
+        --dir|-d)
+            if [ ! -d "$value" ]; then
+                log "an expected dir was not found: '$value' (varname: $keyname) - $info"
+                exit 3
+            fi
+        ;;
+    esac
 }
 
 #test $# -gt 0 && require "$@"
