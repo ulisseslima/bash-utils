1a6d040a8b393b53cb6a112e23b7b4a0ed4a2d39
Ulisses Lima <ulisses@dvlcube.com>
2021-02-04 15:42:34 -0300

    create user/superuser

--- pg_create_user.sh
+++ pg_create_user.sh
@@ -1,52 +1,42 @@
 #!/bin/bash
-# cria um usuário do postgres
+source $(real require.sh)
 
-db="$1"
-if [[ ! -n "$db" ]]; then
-	echo "first argument must be the database"
-	exit 1
-fi
+while test $# -gt 0
+do
+    case "$1" in
+    --user|-u)
+	shift
+      	user=$1
+    ;;
+    --db|-d)
+	shift
+      	database=$1
+    ;;
+    --schema|-s)
+	shift
+      	schema=$1
+    ;;
+    --pass|-p)
+	shift
+      	password=$1
+    ;;
+    -*)
+      echo "bad option '$1'"
+    ;;
+    esac
+    shift
+done
 
-user="${2}"
-if [ ! -n "$user" ]; then
-	echo "second argument must be the user to create"
-	exit 1
-fi
+require user
+require database
+require schema
+require password
 
-password="${3:-pass123}"
-
-schema='public'
-if [[ "$user" == *.* ]]; then
-	schema=$(echo $user | cut -d'.' -f2)
-fi
-
-port="${4:-5432}"
-host="${5:-localhost}"
-
-sql="CREATE USER $user WITH PASSWORD '$password';
-GRANT ALL PRIVILEGES ON DATABASE $db to $user;
-grant all privileges on schema $schema to $user;
-
-grant all privileges on all tables in schema $schema to $user;
-grant all privileges on all sequences in schema $schema to $user;
-
-alter default privileges in schema $schema grant all on tables to $user;
-alter default privileges in schema $schema grant all on sequences to $user;
-
-alter default privileges in schema public grant all on tables to $user;
-alter default privileges in schema public grant all on sequences to $user;
-
-alter user $user with superuser;
+echo "
+CREATE USER $user WITH PASSWORD '$password';
+grant connect on database $database to $user;
+grant usage on schema $schema to $user;
+grant select, insert, update, delete on all tables in schema $schema to $user;
+grant usage, select, update on all sequences in schema $schema to $user;
+grant execute on all functions in schema $schema to $user;
 "
-
-if [[ "$password" == '--update' || "$4" == '--update' ]]; then
-	sql=$(echo "$sql" | tail --lines=+2)
-fi
-
-echo "$sql"
-echo "-------------------------"
-echo "will create user '$user' with password '$password' on schema '$schema' with all privileges on db '$db'"
-echo "press enter to confirm or ctrl c to cancel"
-read confirmation
-
-psql -U postgres -h $host -p $port -c "$sql"
