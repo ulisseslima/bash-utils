01471d3175922ebc67321593eabf8d59813dd0ce
Ulisses Lima <ulisses@dvlcube.com>
2021-03-19 13:39:34 -0300

    cert utils

--- install-cert.sh
+++ install-cert.sh
@@ -1,35 +1,74 @@
-#!/bin/bash
+#!/bin/bash -e
 
-HOST=$1
-if [ ! -n "$1" ]; then
-	echo "first arg must be the host, followed by port on 2nd arg (if not specified, 443 is considered"
+HOST=''
+PORT=443
+KEYSTOREPASS=changeit
+just_check=false
+
+while test $# -gt 0
+do
+    case "$1" in
+    --host|-h)
+      shift
+	  HOST=$1
+    ;;
+	--port|-p)
+		shift
+		PORT=$1
+	;;
+	--java-home)
+		shift
+		JAVA_HOME="$1"
+	;;
+	--keytool)
+		shift
+		kt="$1"
+	;;
+	--cacerts)
+		shift
+		cacerts="$1"
+	;;
+	--cacerts-pass|--passwd)
+		shift
+		KEYSTOREPASS="$1"
+	;;
+    --check)
+	  just_check=true
+    ;;
+    -*) 
+      echo "bad option '$1'"
+    ;;
+    esac
+    shift
+done
+
+if [[ -z "$HOST" ]]; then
+	echo "specify target host with --host"
+	exit 1
+fi
+echo "# using HOST=$HOST"
+echo "# using PORT=$PORT"
+
+echo "# using JAVA_HOME=$JAVA_HOME"
+if [ ! -d "$JAVA_HOME" ]; then
+	echo "JAVA_HOME must be defined as an existing directory: '$JAVA_HOME'"
 	exit 1
 fi
 
-if [[ "$1" == --check ]]; then
-	if [[ ! -n "$2" ]]; then
-		echo "arg 2 must be JAVA_HOME to check"
-		exit 1
-	fi
-
-	keytool -list -keystore "$2/jre/lib/security/cacerts" -storepass changeit
-	exit 0
-fi
-
-echo "JAVA_HOME=$JAVA_HOME"
-if [ ! -n "$JAVA_HOME" ]; then
-	echo "JAVA_HOME must be defined"
-	exit 1
-fi
-
-kt=$(real keytool)
+[[ -z "$kt" ]] && kt="$JAVA_HOME/bin/keytool"
 if [ ! -f "$kt" ]; then
-	echo "keytool nout found: $kt"
+	echo "keytool not found: '$kt'"
 	exit 1
 fi
-echo "keytool: $kt"
+echo "# using keytool=$kt"
 
-existing=$($kt -list -keystore "$JAVA_HOME/jre/lib/security/cacerts" -storepass changeit | grep $HOST)
+if [[ -z "$cacerts" ]]; then
+	cacerts="$(find $JAVA_HOME -name cacerts -print -quit)"
+fi
+echo "# using cacerts=$cacerts"
+echo "# using KEYSTOREPASS=$KEYSTOREPASS"
+
+existing=$($kt -list -keystore "$cacerts" -storepass "$KEYSTOREPASS" | grep $HOST || true)
 if [[ "$existing" == *${HOST}* ]]; then
 	echo "a certificate for $HOST already exists, do you want to remove it? [Y/n]"
 	echo "$existing"
@@ -37,29 +76,32 @@ if [[ "$existing" == *${HOST}* ]]; then
 
 	if [[ "${remove,,}" != 'n' ]]; then
 		echo "removing certificate for $HOST"
-		sudo $kt -delete -alias $HOST -keystore "$JAVA_HOME/jre/lib/security/cacerts" -storepass changeit
+		$kt -delete -alias $HOST -keystore "$cacerts" -storepass "$KEYSTOREPASS"
 	else
 		exit 0
 	fi
 fi
 
-PORT=${2:-443}
-KEYSTOREFILE="$JAVA_HOME/jre/lib/security/cacerts"
-KEYSTOREPASS=changeit
+if [[ "$just_check" == true ]]; then
+	$kt -list -keystore "$cacerts" -storepass $KEYSTOREPASS | grep $HOST
+	exit 0
+fi
 
-echo "getting $HOST:$PORT..."
+RAW_CERT_DIR=$(dirname $cacerts)
+
+echo "will download and install ssl certificate from $HOST:$PORT ..."
 echo "continue?"
 read confirmation
 
 # get the SSL certificate
 openssl s_client -connect ${HOST}:${PORT} </dev/null \
-    | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${HOST}.cert
+    | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $RAW_CERT_DIR/${HOST}.cer
 
 # create a keystore and import certificate
-sudo $kt -import -noprompt -trustcacerts \
-    -alias ${HOST} -file ${HOST}.cert \
-    -keystore ${KEYSTOREFILE} -storepass ${KEYSTOREPASS}
+$kt -import -noprompt -trustcacerts \
+    -alias ${HOST} -file $RAW_CERT_DIR/${HOST}.cer \
+    -keystore ${cacerts} -storepass ${KEYSTOREPASS}
 
 echo ""
 echo "certificate details:"
-$kt -list -keystore "$JAVA_HOME/jre/lib/security/cacerts" -storepass changeit | grep $HOST
+$kt -list -keystore "$cacerts" -storepass $KEYSTOREPASS | grep $HOST || true
