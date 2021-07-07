f1ab898502355ea04de2a22dd96381cf8b5aeece
Ulisses Lima <ulisses@dvlcube.com>
2021-07-06 21:23:13 -0300

    check ssl certificate validity

---
+++ check-ssl-cert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+site="$1"
+if [[ -z "$site" ]]; then
+	echo "arg1 must be site:port"
+	exit 1
+fi
+
+if [[ "$site" != *:* ]]; then
+	site="${site}:443"
+fi
+
+echo "checking certificate from $site ..."
+echo | openssl s_client $site 2>/dev/null | grep Verification
