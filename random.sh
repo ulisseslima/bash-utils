4615f32016b3ac6a0ced5f71ac2443ec92062102
Ulisses Lima <ulisses@dvlcube.com>
2021-07-07 12:16:37 -0300

    zero check

--- random.sh
+++ random.sh
@@ -11,6 +11,11 @@ contents="/tmp/contents"
 cat $in > $contents
 
 n=$(cat "$contents" | wc -l)
+if [[ "$n" -lt 1 ]]; then
+	>&2 echo "file has no lines"
+	exit 1
+fi
+
 random=$(((RANDOM % $n) +1))
 word=$(cat "$contents" | head -$random | tail -1)
 
