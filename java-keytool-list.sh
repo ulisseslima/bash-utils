d94baf14dc3afb29ee5935d646c4fd9a5eb21733
Ulisses Lima <ulisses@dvlcube.com>
2022-01-19 16:21:37 -0300

    keytool list

---
+++ java-keytool-list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+jh="${1:-JAVA_HOME}"
+kt="$jh/bin/keytool"
+cacerts="$jh/jre/lib/security/cacerts"
+
+echo "java home: $jh"
+echo "keytool: $jh"
+echo "cacerts: $cacerts"
+
+$kt -list -keystore $cacerts -storepass changeit
