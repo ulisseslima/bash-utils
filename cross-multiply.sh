030e3c84eddf2bd7e8f1f5a50abcdbe9c28350eb
Ulisses Lima <ulisses@dvlcube.com>
2022-01-19 16:21:17 -0300

    excluding info on std out

--- cross-multiply.sh
+++ cross-multiply.sh
@@ -4,15 +4,20 @@ k1=$1
 k2=$2
 u=$3
 
-echo "if"
-echo "$k1 = $k2"
-echo "then"
-echo "$u = x"
-echo "therefore..."
+info() {
+	>&2 echo "$@"
+}
+
+info "if"
+info "$k1 = $k2"
+info "then"
+info "$u = x"
+info "therefore..."
 
 #u1=$(($u*$k2))
 u1=$(echo "scale=9; $u*$k2" | bc)
 x=$(echo "scale=9; $u1/$k1" | bc)
 
-echo "($u * $k2) / $k1"
-echo "x = $x"
+info "($u * $k2) / $k1"
+info "x ="
+echo $x
