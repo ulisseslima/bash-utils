0f4eb36437cf7d3b39e6da7d4d28eea2d895bc01
Ulisses Lima <ulisses@dvlcube.com>
2022-04-11 21:17:20 -0300

    ignore out

--- git-curr-branch.sh
+++ git-curr-branch.sh
@@ -2,12 +2,12 @@
 
 dir="$1"
 if [[ -d "$dir" ]]; then
-    pushd .
+    pushd . > /dev/null 2>&1
     cd "$dir"
 fi
 
 git rev-parse --abbrev-ref HEAD
 
 if [[ -d "$dir" ]]; then
-    popd
+    popd > /dev/null 2>&1
 fi
