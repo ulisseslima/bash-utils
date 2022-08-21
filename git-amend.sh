7cd339e48e805eb1c81a288c1b1c3c6131b7eaf5
Ulisses Lima <ulisses@dvlcube.com>
2022-08-21 19:28:26 -0300

    git ops

---
+++ git-amend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# use when you forgot to stage some files from the last commit
+git commit --amend --no-edit
--- git-override-and-update.sh
+++ git-override-and-update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-branch="${1:-master}"
+source $(real require.sh)
+
+branch="$1"
+require branch 'branch to override from'
+
 c="git reset --hard origin/$branch"
 echo $c
 $c
