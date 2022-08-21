db95d7c8aa699a3b9b73b5ee5275b9c8c7463586
Ulisses Lima <ulisses@dvlcube.com>
2022-08-21 19:27:54 -0300

    scale option

--- cross-multiply.sh
+++ cross-multiply.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+info() {
+	if [[ $verbose == true ]]; then
+		>&2 echo "$@"
+	fi
+}
+
+verbose=false
+scale=2
+
+if [[ "$1" == -v ]]; then
+	verbose=true
+	shift
+fi
+
+if [[ "$1" == --scale ]]; then
+	shift
+	scale="$1"
+	shift
+fi
+
 k1=$1
 k2=$2
 u=$3
-
-info() {
-	>&2 echo "$@"
-}
 
 info "if"
 info "$k1 = $k2"
@@ -15,8 +31,8 @@ info "$u = x"
 info "therefore..."
 
 #u1=$(($u*$k2))
-u1=$(echo "scale=9; $u*$k2" | bc)
-x=$(echo "scale=9; $u1/$k1" | bc)
+u1=$(echo "scale=$scale; $u*$k2" | bc)
+x=$(echo "scale=$scale; $u1/$k1" | bc)
 
 info "($u * $k2) / $k1"
 info "x ="
