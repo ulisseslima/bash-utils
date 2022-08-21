c43fa45ab554e19c6b7fc29faf0d1b07cdf599a3
Ulisses Lima <ulisses@dvlcube.com>
2022-08-21 19:28:03 -0300

    saible firewall

---
+++ disable-fw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# disable firewall until next login
+
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
