c5d2f6abb7de771a3633e9ea10315f5126c02281
Ulisses Lima <ulisses@dvlcube.com>
2022-12-27 20:11:30 -0300

    more ops

--- dop.sh
+++ dop.sh
@@ -7,7 +7,7 @@ op="$1"
 if [[ -n "$op" ]]; then
 	shift
 else
-	>&2 echo "enter math expression:"
+	>&2 echo "enter date expression (e.g.: 2022-01-01 +days 30):"
 	read op
 fi
 
@@ -21,6 +21,34 @@ do
     --connection|-c)
 	shift
 	psql="$1"
+    ;;
+    --verbose|-v|-d|--debug)
+	shift
+	psql="$1"
+    ;;
+    +minutes)
+	shift
+	op="'${op}'::timestamp +interval '$1 minutes'"
+    ;;
+    +hours)
+	shift
+	op="'${op}'::timestamp +interval '$1 hours'"
+    ;;
+    +days)
+	shift
+	op="'${op}'::timestamp +interval '$1 days'"
+    ;;
+    +weeks)
+	shift
+	op="'${op}'::timestamp +interval '$1 weeks'"
+    ;;
+    +months)
+	shift
+	op="'${op}'::timestamp +interval '$1 months'"
+    ;;
+    +years)
+	shift
+	op="'${op}'::timestamp +interval '$1 years'"
     ;;
     -*)
       echo "bad option '$1'"
