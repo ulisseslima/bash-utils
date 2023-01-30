531b9a0bbacec711da5824e1d270f7de659ed79c
Ulisses Lima <ulisses@dvlcube.com>
2023-01-30 16:11:05 -0300

    sum lines from file

--- sumf.sh
+++ sumf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# sum the values from lines in a file
+# sum the values from lines in a file. ignores comments.
 
-finput="$1"
+finput="${1:-/dev/stdin}"
 
 sum=0
 while read line
 do
 	n=${line//[!0-9.]/}
 	sum=$(echo "scale=2; ${sum}+${n}" | bc)
-done < $finput
+done < <(cat $finput | grep -v '#')
 
 echo $sum
