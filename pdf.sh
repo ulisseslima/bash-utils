bead38295a59844458cca1fc6628034c32a2367c
Ulisses Lima <ulisses@dvlcube.com>
2023-01-30 16:10:46 -0300

    args and landscape

--- pdf.sh
+++ pdf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# pdf from text (stdin)
 
 require() {
     switch='-s'
@@ -17,11 +18,27 @@ require() {
     esac
 }
 
-if [[ "$1" == *-h* ]]; then
+while test $# -gt 0
+do
+    case "$1" in
+    --help|-h)
 	echo "creates a PDF from text."
-	echo "usage: "
-	echo "echo text | pdf.sh"
-fi
+        echo "usage:"
+        echo "echo text | pdf.sh"
+      exit 0
+    ;;
+    --verbose|-v)
+      verbose=true
+    ;;
+    --landscape)
+      options="$options --landscape"
+    ;;
+    -*)
+      echo "bad option '$1'"
+    ;;
+    esac
+    shift
+done
 
 now=$(date +'%Y-%m-%d_%H-%M-%S')
 fname="pdf-${now}"
@@ -29,7 +46,7 @@ fname="pdf-${now}"
 require -f $(which enscript) lib
 require -f $(which ps2pdf) lib
 
-enscript -p "${fname}.ps"
+enscript -v -X latin1 $options -p "${fname}.ps"
 ps2pdf "${fname}.ps"
 
 rm "${fname}.ps"
