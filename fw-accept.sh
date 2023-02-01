58c1590b6ac474192d11dc34338ec3e9e7658dd9
Ulisses Lima <ulisses@dvlcube.com>
2023-02-01 11:18:24 -0300

    require port

--- fw-accept.sh
+++ fw-accept.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # accept data through a port
 
+source $(real require.sh)
+
 port="$1"
+require port
+
 sudo iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT
