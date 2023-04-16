b91722a1df926c85cc5c8dc7288c35c6574469d4
Ulisses Lima <ulisses@dvlcube.com>
2023-04-16 02:30:28 -0300

    timer

--- elapsed.sh
+++ elapsed.sh
@@ -4,12 +4,22 @@
 # elapsed.sh $start
 
 start="$1"
-if [ ! -n "$start" ]; then
-	echo $(($(date +%s%N)/1000000))
+if [[ -z "$start" ]]; then
+	millis=$(($(date +%s%N)/1000000))
+	echo "$millis"
+
 	exit 0
 fi
 
 end=$(($(date +%s%N)/1000000))
 elapsed_ms=$(($end-$start))
 
-echo $elapsed_ms
+if [[ "$2" == -s ]]; then
+	echo $((elapsed_ms/1000))
+elif [[ "$2" == --minutes ]]; then
+	echo $((elapsed_ms/1000/60))
+elif [[ "$2" == --hours ]]; then
+	echo $((elapsed_ms/1000/60/60))
+else
+	echo $elapsed_ms
+fi
