bc400ecbcb8b306937b80383758fb6b5ef97e348
Ulisses Lima <ulisses@dvlcube.com>
2023-07-11 11:22:22 -0300

    improvements

--- word-wrap.sh
+++ word-wrap.sh
@@ -6,7 +6,7 @@ if [[ ! -f "$1" ]]; then
   exit 1
 fi
 
-max_characters_per_line=${2:-20}
+max_characters_per_line=${2:-22}
 
 # Read input file and wrap lines
 while read -r line; do
@@ -14,11 +14,17 @@ while read -r line; do
     # Find the last space character within the first $max_characters_per_line characters of the line
     wrap_index=$(echo "${line:0:$max_characters_per_line}" | sed -n 's/[^ ]* *$//p' | wc -c)
     wrap_index=$((wrap_index-1))
-    # Output the first part of the line (up to the wrap index)
-    echo "${line:0:$wrap_index}"
-    # Remove the first part of the line (up to the wrap index)
-    line="${line:$wrap_index}"
+    if [[ $wrap_index == 0 ]]; then
+      # no spaces (word too long) or space too late
+      echo "${line:0:$max_characters_per_line}-"
+      line="${line:$max_characters_per_line}"
+    else  
+      # Output the first part of the line (up to the wrap index)
+      echo "${line:0:$wrap_index}"
+      # Remove the first part of the line (up to the wrap index)
+      line="${line:$wrap_index}"
+    fi
   done
-  # Output any remaining characters on the line (less than 40 characters)
+  # Output any remaining characters on the line (less than $max_characters_per_line characters)
   echo "$line"
 done < "$1"
