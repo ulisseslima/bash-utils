9a3a54e470d99996717b84d5ae9561856fe11245
Ulisses Lima <ulisses@dvlcube.com>
2023-08-10 17:07:31 -0300

    create group if it doesn't exist

--- add-group.sh
+++ add-group.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
 # adds a group to current user
 
 group="$1"
 user=${2:-$USER}
+
+if [ $(getent group $group) ]; then
+    getent group $group
+else
+    echo "group '$group' does not exist. create?"
+    read confirmation
+    groupadd $group
+fi
 
 echo "adding '$user' to '$group'..."
 sudo usermod -aG $group $user
