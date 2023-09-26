a5bc5b3b5e9b2129b2b075c7eb7a54cec1b58780
Ulisses Lima <ulisses@dvlcube.com>
2023-09-25 21:55:43 -0300

    check d

--- java-version.sh
+++ java-version.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/7334754/correct-way-to-check-java-version-from-bash-script/56243046#56243046
 
-if [ -n "$1" ]; then
+source $(real require.sh)
+
+if [[ -n "$1" ]]; then
 	JAVA_HOME="$1"
 fi
+
+require -d JAVA_HOME
 
 "$JAVA_HOME/bin/java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '0,/^1\./s///' | cut -d'.' -f1
