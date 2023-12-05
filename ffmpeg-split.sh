cd8f17ae7129f714f0f7bfb8fdaf9daafbc40471
ulisses <ulisses@dvlcube.com>
2023-12-05 12:45:44 -0300

    split video into fixed time segments

---
+++ ffmpeg-split.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# splits a file into various segments with the specified seconds
+
+source $(real require.sh)
+
+input=$1
+require -f input "input file"
+shift
+
+extension=$(basename "$input" | rev | cut -d'.' -f1 | rev)
+
+seconds=$1
+require seconds "segment size"
+
+if [[ $seconds == half ]]; then
+	seconds=$(op.sh $(ffmpeg-info.sh $input duration)/2)
+	>&2 echo "half segment: $seconds seconds"
+	require seconds "half duration calculation"
+fi
+
+ffmpeg -i $input -f segment -segment_time $seconds -reset_timestamps 1 -c copy ${input}-segment-%03d.$extension
