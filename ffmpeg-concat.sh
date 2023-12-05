1bd72bfcd4d8924b0e479f51517fa5767f2b7de6
ulisses <ulisses@dvlcube.com>
2023-12-05 12:45:32 -0300

    safe path

--- ffmpeg-concat.sh
+++ ffmpeg-concat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/7333232/how-to-concatenate-two-mp4-files-using-ffmpeg
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
 
-ffmpeg -safe 0 -f concat -i <(find . -type f -name "*-video.mp4" -printf "file '$PWD/%p'\n" | sort) -c copy "concat.mp4"
+start_dir=${1:-$MYDIR}
+pattern=${2:-.mp4}
 
+ffmpeg -safe 0 -f concat -i <(find $start_dir -type f -name *"$pattern" -printf "file '$PWD/%p'\n" | sort) -c copy "result${pattern}"
