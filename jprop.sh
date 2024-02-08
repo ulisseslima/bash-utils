d9615f0368ca33baf74f19f2da0a3fca9ef4ca2a
ulisses <ulisses@dvlcube.com>
2024-02-07 22:18:34 -0300

    support both py versions

--- jprop.sh
+++ jprop.sh
@@ -2,7 +2,11 @@
 export PYTHONIOENCODING=utf-8
 
 py=$(which python2)
-[[ -z "$py" ]] && py=$(which python)
-
-# TODO detect and support python3
-cat /dev/stdin | $py -c "import sys, json; print json.load(sys.stdin)$1"
+if [[ -n "$py" ]]; then
+  cat /dev/stdin | $py -c "import sys, json; print json.load(sys.stdin)$1"
+else
+  py=$(which python)
+  [[ -z "$py" ]] && py=python3
+  # end='' -> remove this to include a line break
+  cat /dev/stdin | $py -c "import sys, json; y = json.load(sys.stdin); print(y$1, end='')"
+fi
