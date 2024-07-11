5d0a66386b1cfedd976025bb0d425dc3a1ab7aa8
ulisses <ulisses@dvlcube.com>
2024-07-11 12:22:09 -0300

    no resize if already $resize

--- ffmpeg-thumbnail.sh
+++ ffmpeg-thumbnail.sh
@@ -13,10 +13,34 @@ require frame 'desired timestamp. e.g.: 00:00:14.435'
 video_dir=$(dirname "$video")
 
 out="$video_dir/thumb-$frame.png"
-ffmpeg -i "$video" -ss $frame -frames:v 1 "$out"
+resize=720
 
-height=720
-redim="$video_dir/thumb-${frame}-${height}p.png"
-convert -resize x${height} "$out" "$redim"
+while test $# -gt 0
+do
+    case "$1" in
+        --out|-o)
+	    shift
+            out="$1"
+        ;;
+        --resize)
+            shift
+	    resize=$1
+        ;;
+        -*)
+            echo "unrecognized option: $1"
+            exit 1377
+        ;;
+    esac
+    shift
+done
+
+ffmpeg -v 16 -y -i "$video" -ss $frame -frames:v 1 "$out"
+
+height=$(file "$out" | cut -d' ' -f7 | tr -d ',')
+if [[ "$height" != "$resize"  ]]; then
+	redim="$video_dir/thumb-${frame}-${resize}p.png"
+	convert -resize x${resize} "$out" "$redim"
+	>&2 echo "${resize}p: $redim"
+fi
 
 echo "$redim"
