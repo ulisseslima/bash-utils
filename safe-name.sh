f639bbd55ed1ad64a4ecccc77d3df08acb2ca045
ulisses <ulisses@dvlcube.com>
2024-09-27 11:56:26 -0300

    docs and remove non alpha

--- safe-name.sh
+++ safe-name.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 function safe_name() {
+    # remove non ascii:
     name=$(echo "$1" | iconv -f utf8 -t ascii//TRANSLIT)
+    limit=${2:-100}
+
+    # to lower case:
     name=$(echo ${name,,})
-    echo ${name// /-}
+    # replace spaces for "-", then remove anything that's non alphanumeric
+    safen=$(echo ${name// /-} | sed 's/[^a-z0-9-]//g')
+    echo "${safen:0:$limit}"
 }
 
 safe_name "$1"
