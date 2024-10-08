df977b438412dcf5180a1d940f7e8afd74d24811
ulisses <ulisses@dvlcube.com>
2024-10-08 01:59:35 -0300

    cleaner queue

--- at-queue.sh
+++ at-queue.sh
@@ -2,6 +2,6 @@
 # detail ALL `at` jobs
 while read job
 do
-	job_id=$(echo "$job" | awk '{print $1}')
+	job_id=$(echo "$job" | awk '{print $2}')
 	echo "$job | $(at-content.sh $job_id)"
-done < <(atq)
+done < <(atq -o "%Y-%m-%d %H:%M:%S" | tr '\t' ' ' | awk '{print $2,$1}' | sort)
