b387fad434a04558a1589b56c770c951b5b8f111
ulisses <ulisses@dvlcube.com>
2025-03-03 10:06:43 -0300

    clean stale local branches

--- git-rm-branch.sh
+++ git-rm-branch.sh
@@ -17,4 +17,20 @@ git branch -d $branch
 if [[ "$remote" == true ]]; then
     echo "deleting branch remotely..."
     git push origin --delete $branch
+else
+    remote_exists=$(git branch -r | grep -c $branch || true)
+    if [[ "$remote_exists" -gt 0 ]]; then
+	echo "remote branch found. delete? $(git branch -r | grep $branch)"
+	read confirmation
+	if [[ ${confirmation,,} == y* ]]; then
+	    echo "deleting branch remotely..."
+    	    git push origin --delete $branch
+	fi
+    fi
 fi
+
+# https://unix.stackexchange.com/questions/441442/tab-completion-for-git-branches-showing-old-outdated-entries
+git remote prune origin
+
+# https://stackoverflow.com/questions/18308535/automatic-prune-with-git-fetch-or-pull
+git fetch --prune
