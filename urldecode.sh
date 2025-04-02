ad89073995672e44207920e3c5054ae8701b332e
ulisses <ulisses@dvlcube.com>
2025-04-02 16:41:15 -0300

    decode url

--- urldecode.sh
+++ urldecode.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+# Check if the user supplied an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <url_encoded_string>"
+    exit 1
+fi
 
-urldecode $(cat /dev/stdin)
+# Function to URL decode using printf
+url_decode() {
+    local url_encoded=${1//+/ } # Plus (+) signs usually represent spaces
+    printf '%b' "${url_encoded//%/\\x}"
+}
 
+# Decode and output the decoded URL
+url_decode "$1"
+echo
