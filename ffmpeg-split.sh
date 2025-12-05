7d3b253dcc1ad278a1c0fcb5c46df4251a9d5199
ulisseslima <ulisses@dvlcube.com>
2025-12-05 14:39:18 -0300

    quotes

--- ffmpeg-split.sh
+++ ffmpeg-split.sh
@@ -3,9 +3,12 @@
 
 source $(real require.sh)
 
-input=$1
+input="$1"
 require -f input "input file"
 shift
+
+odir=$(dirname "$input")
+cd "$odir"
 
 extension=$(basename "$input" | rev | cut -d'.' -f1 | rev)
 
@@ -18,4 +21,4 @@ if [[ $seconds == half ]]; then
 	require seconds "half duration calculation"
 fi
 
-ffmpeg -i $input -f segment -segment_time $seconds -reset_timestamps 1 -c copy ${input}-segment-%03d.$extension
+ffmpeg -i "$input" -f segment -segment_time $seconds -reset_timestamps 1 -c copy "${input}-segment-%03d.$extension"
