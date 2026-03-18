f1ffa76d96ad1f92454d49d9bff4f34fa8e8894b
ulisseslima <ulisses@dvlcube.com>
2026-03-18 14:36:23 -0300

    git utils

---
+++ git-config-pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# least complicated pull option. any merge conflicts are resolved locally.
+
+git config pull.ff false
--- git-unsave-credentials.sh
+++ git-unsave-credentials.sh
@@ -4,3 +4,6 @@ MYDIR="${MYSELF%/*}"
 ME=$(basename $MYSELF)
 
 git config --global --unset credential.helper
+
+echo "remove ~/.git-credentials manually:"
+cat ~/.git-credentials
