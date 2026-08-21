407be775aa6e9cdf3a884e4f8cd709c4c5c04212
ulisseslima <ulisses@dvlcube.com>
2026-08-20 22:49:06 -0300

    check merge commit

--- git-cherry-pick.sh
+++ git-cherry-pick.sh
@@ -6,7 +6,20 @@ source $(real require.sh)
 commit="$1"
 require commit
 
-# note: if it fails, try specifying -m 1 before the commit hash
 # note: if "fatal: bad object", try git fetch
-git pull
-git cherry-pick $commit
+git pull || exit $?
+
+cherry_pick_output="$(git cherry-pick "$commit" 2>&1)"
+cherry_pick_status=$?
+echo "$cherry_pick_output"
+
+if [ $cherry_pick_status -ne 0 ] && echo "$cherry_pick_output" | grep -q "is a merge but no -m option was given"; then
+  read -r -p "This commit appears to be a merge commit. Retry with '-m 1'? [y/N] " retry_as_merge
+
+  if [[ "$retry_as_merge" =~ ^[Yy]$ ]]; then
+    git cherry-pick -m 1 "$commit"
+    exit $?
+  fi
+fi
+
+exit $cherry_pick_status
